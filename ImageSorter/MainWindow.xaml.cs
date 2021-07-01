e436d036dcab04cb68afb565bc7a1ff41001f572
Silverback1992 <44880539+Silverback1992@users.noreply.github.com>
2021-07-01 15:38:55 +0200

    Initial commit

---
+++ ImageSorter/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Ookii.Dialogs.Wpf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImageSorter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void sourceFolderSelectorButton_Click(object sender, RoutedEventArgs e) => Utility.SelectFolderAndDisplayInBox(this, sourceFolderPathTextBox);
+
+        private void targetFolderSelectorButton_Click(object sender, RoutedEventArgs e) => Utility.SelectFolderAndDisplayInBox(this, targetFolderPathTextBox);
+
+        private void exitButton_Click(object sender, RoutedEventArgs e) => Close();
+
+        private void processButton_Click(object sender, RoutedEventArgs e)
+        {
+            string sourceFoPath = sourceFolderPathTextBox.Text;
+            string targetFoPath = targetFolderPathTextBox.Text;
+            bool? isSearchSubFolders = subdirectorySearch.IsChecked;
+
+            if(String.IsNullOrWhiteSpace(sourceFoPath) || String.IsNullOrWhiteSpace(targetFoPath))
+            {
+                MessageBox.Show("Please provide a source path and target path first.");
+            }
+            else
+            {
+                var sourceFo = new DirectoryInfo(sourceFoPath);
+                var targetFo = new DirectoryInfo(targetFoPath);
+
+                var t = new Task(() =>
+                {
+                    Utility.CopyFiles(sourceFo, targetFo, isSearchSubFolders, this);
+                    MessageBox.Show("Process finished.");
+                    Dispatcher.Invoke(() => { processProgressbar.Value = 0; });
+                });
+
+                t.Start();
+            }
+        }
+    }
+}
---
+++ ImageSorter/Utility.cs
@@ -0,0 +1,85 @@
+﻿using Ookii.Dialogs.Wpf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace ImageSorter
+{
+    public static class Utility
+    {
+        public static void SelectFolderAndDisplayInBox(MainWindow mw, TextBox tb)
+        {
+            var dialog = new VistaFolderBrowserDialog();
+            if (dialog.ShowDialog(mw).GetValueOrDefault())
+            {
+                tb.Text = dialog.SelectedPath;
+            }
+        }
+
+        public static void CopyFiles(DirectoryInfo sourceFo, DirectoryInfo targetFo, bool? isSearchSubfolders, MainWindow mw)
+        {
+            var files = sourceFo.EnumerateFiles();
+
+            if (isSearchSubfolders ==  true)
+            {
+                var directories = sourceFo.EnumerateDirectories();
+
+                OrganizeFiles(files, targetFo, mw);
+
+                foreach (var directory in directories)
+                {
+                    CopyFiles(directory, targetFo, true, mw);
+                }
+            }
+            else
+            {
+                OrganizeFiles(files, targetFo, mw);
+            }
+        }
+
+        private static void OrganizeFiles(IEnumerable<FileInfo> files, DirectoryInfo targetFo, MainWindow mw)
+        {
+            foreach (var f in files)
+            {
+                string lastModifiedYear = f.LastWriteTime.Year.ToString();
+                string lastModifiedMonth = f.LastWriteTime.Month.ToString();
+
+                var finalFo = GetFinalFolder(targetFo, lastModifiedYear, lastModifiedMonth);
+                var finalFullFileName = $"{finalFo.FullName}\\{f.Name}";
+
+                if(File.Exists(finalFullFileName))
+                {
+                    int i = 1;
+
+                    finalFullFileName = $"{finalFo.FullName}\\{Path.GetFileNameWithoutExtension(f.Name)}_{i}{Path.GetExtension(f.Name)}";
+                    
+                    while (File.Exists(finalFullFileName))
+                    {
+                        i++;
+                        finalFullFileName = $"{finalFo.FullName}\\{Path.GetFileNameWithoutExtension(f.Name)}_{i}{Path.GetExtension(f.Name)}";
+                    }
+                }
+
+                File.Copy(f.FullName, finalFullFileName);
+                mw.Dispatcher.Invoke(() => { mw.processProgressbar.Value++; });
+            }
+        }
+
+        private static DirectoryInfo GetFinalFolder(DirectoryInfo targetFo, string lastModifiedYear, string lastModifiedMonth)
+        {
+            string finalFolderName = $"{lastModifiedYear}_{lastModifiedMonth}";
+            var finalFolder = targetFo.EnumerateDirectories().FirstOrDefault(x => x.Name == finalFolderName);
+
+            if (finalFolder == null)
+                finalFolder = Directory.CreateDirectory($"{targetFo.FullName}\\{finalFolderName}");
+
+            return finalFolder;
+        }
+    }
+}
